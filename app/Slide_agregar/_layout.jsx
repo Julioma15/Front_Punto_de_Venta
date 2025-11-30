09266163ef22e1b0e0ff5cac688e32f4cf77cb53
Zelio-1 <miguelruzagu@hotmail.com>
2025-11-30 15:48:46 -0600

    MiguelRuz_Actualización_30/11/2025_03:48PM

---
+++ app/Slide_agregar/_layout.jsx
@@ -0,0 +1,14 @@
+import { Stack } from 'expo-router';
+
+const SlideAgregar_Layout = ()=>{
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false //Que no muestre otro menu. Unicamente aparece el del index.jsx
+            }}    
+
+        />
+    )
+}
+
+export default SlideAgregar_Layout
